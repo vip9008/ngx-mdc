6abb4e357d5d191d2c62e5af63861f80b628ebf4
Hussain Almoamen <ave9008@gmail.com>
2025-02-07 02:46:49 +0300

    update

--- mdc/mdc-menu/mdc-select-menu/mdc-select-menu.component.ts
+++ mdc/mdc-menu/mdc-select-menu/mdc-select-menu.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MdcMenuComponent } from '../mdc-menu.component';
 import { AbstractControl, FormControl } from '@angular/forms';
+import { find, cloneDeep, filter } from 'lodash-es';
 
 @Component({
     standalone: false,
@@ -11,7 +12,7 @@ import { AbstractControl, FormControl } from '@angular/forms';
         class: 'mdc-select-menu'
     }
 })
-export class MdcSelectMenuComponent implements AfterViewInit {
+export class MdcSelectMenuComponent implements OnInit, AfterViewInit {
     @ViewChild(MdcMenuComponent) menuComponent: MdcMenuComponent;
 
     @Input() control: FormControl | AbstractControl;
@@ -20,8 +21,51 @@ export class MdcSelectMenuComponent implements AfterViewInit {
     @Input() appearance: 'default' | 'standard' | 'outlined' = 'default';
     @Input() label: string = '';
     @Input() disabled: boolean = false;
+    @Input() searchable: boolean = false;
+    @Input() notFoundText: string;
+    @Input() options: {
+        label: string,
+        value: string | number | boolean,
+    }[] = [];
+
+    public selectedLabel: string = '';
+    public selectedValue: string | number | boolean;
+    public filteredOptions: {
+        label: string,
+        value: string | number | boolean,
+    }[] = [];
+
+    ngOnInit(): void {
+        this.filteredOptions = cloneDeep(this.options);
+    }
 
     ngAfterViewInit(): void {
+        this.notFoundText = this.notFoundText?.length ? this.notFoundText : 'Can\'t find any item!';
         this.menuComponent.element.nativeElement.classList.add('select-menu');
+        let selected = this.control?.value;
+        if (selected && selected?.toString().length) {
+            let selectedOption = find(this.options, (item) => item.value == selected);
+            this.selectedLabel = selectedOption?.label;
+            this.selectedValue = selectedOption?.value;
+        }
+    }
+
+    public selectOption(option: {
+        label: string,
+        value: string | number | boolean,
+    }) {
+        this.control?.setValue(option.value);
+
+        this.selectedLabel = option?.label;
+        this.selectedValue = option?.value;
+    }
+
+    public searchOptions(event: Event) {
+        let searchText: string = (event.target as HTMLInputElement)?.value;
+        this.filteredOptions = this.filteredOptions = (!searchText || searchText.length == 0) ? cloneDeep(this.options) : filter(this.options, (option: { label: string, value: string | number | boolean }) => {
+            let labelMatch = option.label.toLowerCase().includes(searchText.toLowerCase());
+            let valueMatch = typeof option.value === 'string' && option.value.toLowerCase().includes(searchText.toLowerCase());
+            return labelMatch || valueMatch;
+        });
     }
 }
