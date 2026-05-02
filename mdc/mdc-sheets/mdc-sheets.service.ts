e78deaea9eed7289dc79bbc9d8c721de7bd5f9b6
vip9008 <ave9008@gmail.com>
2026-05-02 04:12:59 +0300

    Update mdc-sheets.service.ts

--- mdc/mdc-sheets/mdc-sheets.service.ts
+++ mdc/mdc-sheets/mdc-sheets.service.ts
@@ -3,6 +3,7 @@ import { MdcSideSheetComponent } from './mdc-side-sheet/mdc-side-sheet.component
 import { EventType, Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { MdcLayoutService } from '../mdc-layout/mdc-layout.service';
+import { MdcHostDirective } from '../mdc-common/mdc-host.directive';
 
 @UntilDestroy()
 @Injectable({
@@ -25,28 +26,31 @@ export class MdcSheetsService {
 
     get rootViewContainerRef(): ViewContainerRef {
         const appInstance = this.applicationRef.components[0].instance;
+        const mdcHost: MdcHostDirective = appInstance.mdcComponentContainer;
     
-        if (!appInstance.mdcComponentContainer) {
+        if (!mdcHost) {
             const appName = this.applicationRef.componentTypes[0].name;
             throw new Error(`Missing @ViewChild(MdcHostDirective) declaration in ${appName}`);
         }
     
-        return appInstance.mdcComponentContainer.viewContainerRef;
+        return mdcHost.viewContainerRef;
     }
 
     private get currentSideSheet(): ComponentRef<MdcSideSheetComponent> {
         return this.sideSheetRef;
     }
 
-    public createSideSheet<T = any>(dynamicComponent: any, type: 'above-content' | 'coplanar' | 'modal' = 'above-content', data: Object = {}): ComponentRef<MdcSideSheetComponent<T>> | null {
+    public createSideSheet<T = any>(dynamicComponent: any, type: 'above-content' | 'coplanar' | 'modal' = 'above-content', data: Object = {}, mdcHost: MdcHostDirective = null): ComponentRef<MdcSideSheetComponent<T>> | null {
         if (this.currentSideSheet) {
             return null;
         }
 
-        const componentRef: ComponentRef<T> = this.rootViewContainerRef.createComponent(dynamicComponent) as ComponentRef<T>;
+        const vcr: ViewContainerRef = mdcHost ? mdcHost.viewContainerRef : this.rootViewContainerRef;
+
+        const componentRef: ComponentRef<T> = vcr.createComponent(dynamicComponent) as ComponentRef<T>;
         Object.assign(componentRef.instance, data);
 
-        const sheetRef: ComponentRef<MdcSideSheetComponent<T>> = this.rootViewContainerRef.createComponent(MdcSideSheetComponent, {
+        const sheetRef: ComponentRef<MdcSideSheetComponent<T>> = vcr.createComponent(MdcSideSheetComponent, {
             projectableNodes: [
                 [componentRef.location.nativeElement]
             ]
